2895e9657dbfff7587de1ee6be800b5e6ffcfdc4
higi <cdramirezh@unal.edu.co>
2022-11-21 20:51:02 -0500

    Added test views to future test implementation

---
+++ network/tests/test_Views.py
@@ -0,0 +1,12 @@
+from django.test import Client ,TestCase
+from ..views import *
+
+class ViewTestCase():
+
+    def setUp(self):
+        pass
+
+    # def create_post():
+    #     client = Client()
+        
+
